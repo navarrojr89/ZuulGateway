1d6c4c9a4a1701ad56eb91bd46bcf1d974f3a043
Luis Navarro <navarrojr89@gmail.com>
2021-02-22 19:24:11 +0100

    Includes the configuration from the global one

--- src/main/java/com/lamn/microservices/zuulgateway/oauth/ResourceServerConfig.java
+++ src/main/java/com/lamn/microservices/zuulgateway/oauth/ResourceServerConfig.java
@@ -1,5 +1,7 @@
 package com.lamn.microservices.zuulgateway.oauth;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -15,7 +17,11 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
  */
 @Configuration
 @EnableResourceServer
+@RefreshScope
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Value("${config.security.oauth.jwt.key}")
+    private String jwtKey;
 
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
@@ -48,7 +54,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Bean
     public JwtAccessTokenConverter accessTokenConverter() {
         JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-        tokenConverter.setSigningKey("secret_code_for_signin");
+        tokenConverter.setSigningKey(jwtKey);
         return tokenConverter;
     }
 
